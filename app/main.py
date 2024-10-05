8aef1d8a5f3f0ae125f22bc167bd3fcaa9cc1a25
Mark <strategy155@gmail.com>
2024-10-05 17:44:19 +0200

    added prod ready confs

--- app/main.py
+++ app/main.py
@@ -11,6 +11,6 @@ app.include_router(exoplanets.router)
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8090))
 
     uvicorn.run(app, host="0.0.0.0", port=port)
