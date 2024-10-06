8bb5ec7cd40dcf420a815a473d1473b1e7702a87
Mark <strategy155@gmail.com>
2024-10-07 01:35:20 +0200

    added cors

--- app/main.py
+++ app/main.py
@@ -4,7 +4,28 @@ from fastapi import FastAPI
 from app.constants import FAPI_ROOT_PATH
 from app.routers import stars, exoplanets
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "*"
+]
+
+
+
+
+
 app = FastAPI(root_path=FAPI_ROOT_PATH)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(stars.router)
 app.include_router(exoplanets.router)
