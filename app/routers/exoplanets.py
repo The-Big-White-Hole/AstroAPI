25c828bd5d47438d16d4db1b732543a1a1b57578
Mark <strategy155@gmail.com>
2024-10-07 01:21:48 +0200

    added route for printing

--- app/routers/exoplanets.py
+++ app/routers/exoplanets.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
-from app.constants import EXOPLANETS_FILE_PATH
+from app.constants import EXOPLANETS_FILE_PATH, DATA_PATH
 from app.fixtures.collector import MOCKED_EXOPLANETS
 from app.models import Exoplanet, Star
 import pandas as pd
+from fastapi.responses import FileResponse
+
 
 router = APIRouter()
 
@@ -15,3 +17,9 @@ async def get_exoplanets():
     """
     planets_t = pd.read_csv(EXOPLANETS_FILE_PATH)
     return planets_t.to_dict()
+
+@router.get("/exoplanets/{planet_name}/print")
+async def get_exoplanet_pdf(planet_name: str):
+    planet_namepath = planet_name + ".pdf"
+    filepath = DATA_PATH / planet_namepath
+    return FileResponse(filepath)
