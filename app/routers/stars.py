585f7947e5fc281607fafcd57ad06e3d78b8511f
Mark <strategy155@gmail.com>
2024-10-07 01:09:34 +0200

    added needed tooling for backend here

--- app/routers/stars.py
+++ app/routers/stars.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Query
 
+from app.constants import DATA_PATH
 from app.fixtures.collector import MOCKED_STARS
 from app.models import Star
+import pandas as pd
 
 router = APIRouter()
 
@@ -11,4 +13,7 @@ def get_stars(planet_name: str):
     """
     Returns array of stars and their coordinates.
     """
-    return {"mocha": planet_name}
+    planet_namepath = planet_name + ".json"
+    filepath = DATA_PATH / planet_namepath
+    star_file = pd.read_json(filepath)
+    return star_file.to_dict()
