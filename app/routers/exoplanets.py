7849357f2184bd1bac31aa518d092faa2761dcc2
Nikita Rudakov <nuromirg@gmail.com>
2024-10-07 02:18:24 +0200

    added path for json planet response

--- app/routers/exoplanets.py
+++ app/routers/exoplanets.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from app.constants import EXOPLANETS_FILE_PATH, DATA_PATH
 from app.fixtures.collector import MOCKED_EXOPLANETS
 from app.models import Exoplanet, Star
 import pandas as pd
 from fastapi.responses import FileResponse
+import json
 
 
 router = APIRouter()
@@ -23,3 +24,16 @@ async def get_exoplanet_pdf(planet_name: str):
     planet_namepath = planet_name + ".pdf"
     filepath = DATA_PATH / planet_namepath
     return FileResponse(filepath)
+
+@router.get("/exoplanets/{name}")
+async def get_exoplanet_stars(name: str):
+    filename = name + ".json" 
+    path = DATA_PATH / filename
+
+    if not path.exists():
+        raise HTTPException(status_code=404, detail="File not found")
+
+    with open(path, "r") as file:
+        json_data = json.load(file)
+
+    return json_data
